912415138741c9eed8932de82be9dab1b0f24267
Danindunadith <117801278+Danindunadith@users.noreply.github.com>
2024-02-04 11:07:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import AddStudent from './components/AddStudent';
+import CounterClass from './components/CounterClass';
+import CounterFunction from './components/CounterFunction';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    
+      <div>
+        
+        <Header/>
+        
+        <AddStudent/>
+      </div>
+  );
+}
+
+export default App;
